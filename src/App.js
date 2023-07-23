0330e0418ee8af46a2981b3e2fb596b7b2b86aed
shivangi <shivangivv07@gmail.com>
2023-07-23 23:36:40 +0530

    react music player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import Player from "./Player";
+import './App.css'
 function App() {
+  const [songs] = useState([
+    {
+      title: "Titaliya",
+      artist: "Hardy Sandhu",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134942/assist/Ganeshji_gld5pr.jpg",
+      src: "./music/Titliaan Warga.mp3",
+    },
+    {
+      title: "Nach Meri Rani",
+      artist: "Yo Yo Honey Singh",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134945/assist/RadhaKishna_ckn7zn.jpg",
+      src: "./music/Naach Meri Rani.mp3",
+    },
+    {
+      title: "Care Ni Karda",
+      artist: "Yo Yo Honey Singh",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134947/assist/Saraswati_syvnaf.jpg",
+      src: "./music/Care Ni Karda.mp3",
+    },
+    {
+      title: "Burj Khalifa",
+      artist: "Shashi",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134972/assist/Shivji_hrloje.jpg",
+      src: "./music/BurjKhalifa.mp3",
+    },
+    {
+      title: "Tango Del Fuego",
+      artist: "Parov Stelar",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134972/assist/Shivji_hrloje.jpg",
+      src: "./music/ParovStelarGeorgiaGibbs-TangoDelFuego.mp3",
+    },
+    {
+      title: "Take You Dancing",
+      artist: "Jason Derulo",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134947/assist/Saraswati_syvnaf.jpg",
+      src: "./assets/Songs/Gayatri Mantra.mp3",
+    },
+    {
+      title: "Daisy",
+      artist: "Ashnikko",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134945/assist/RadhaKishna_ckn7zn.jpg",
+      src: "./music/Ashnikko-Daisy.mp3",
+    },
+    {
+      title: "Dolly Song",
+      artist: "Partz Grimbad",
+      img_src: "https://res.cloudinary.com/dn6uh1lxj/image/upload/v1690134972/assist/Shivji_hrloje.jpg",
+      src: "./music/PatzGrimbard-DollySong.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
---
+++ src/Controls.js
@@ -0,0 +1,23 @@
+
+import { FaPlay, FaPause, FaForward, FaBackward } from 'react-icons/fa';
+
+function Controls(props) {
+  return (
+    <div className="c-player--controls">
+      <button className="skip-btn" onClick={() => props.SkipSong(false)}>
+        <FaBackward />
+      </button>
+      <button
+        className="play-btn"
+        onClick={() => props.setIsPlaying(!props.isPlaying)}
+      >
+        <FaPlay icon={props.isPlaying ? FaPause : FaPlay} />
+      </button>
+      <button className="skip-btn" onClick={() => props.SkipSong()}>
+        <FaForward />
+      </button>
+    </div>
+  );
+}
+
+export default Controls;
